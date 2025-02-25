05a9bf6c58f1bf28477f515c570ad0828cf65313
Luis Rodríguez <luisrodriguezcamas99@gmail.com>
2025-02-25 13:08:22 +0100

    Creación Clase "Dados"

--- src/model/Dados.java
+++ src/model/Dados.java
@@ -1,5 +1,42 @@
 package model;
 
+import java.util.Random;
+
 public class Dados {
+
+	private int valorDado;
+
+	// CONSTRUCTOR COMPLETO
+	public Dados(int valorDado) {
+		this.valorDado = valorDado;
+	}
+
+	// Constructor vacío
+	public Dados() {
+		valorDado = 1;
+	}
+
+	// SETTERS & GETTERS
+	public int getValorDado() {
+		return valorDado;
+	}
+
+	public void setValorDado(int valorDado) {
+		this.valorDado = valorDado;
+	}
+
+	// MÉTODOS VARIOS
+
+	// Devuelve un valor aleatorio entre 1 y 6
+	public int generateRandom(int min, int max) {
+		Random rand = new Random();
+		return rand.nextInt(max - min + 1) + min;
+
+	}
+
+	public void tirarDado() {
+		valorDado = generateRandom(1, 6);
+		System.out.println("Tiras el dado y sacas un: " + valorDado);
+	}
 
 }
