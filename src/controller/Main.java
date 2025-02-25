342cf86f78e383a895690714e0997be8065d429c
Luis Rodríguez <luisrodriguezcamas99@gmail.com>
2025-02-25 13:35:09 +0100

    Creación Clase Jugador (WIP)
    Si se ejecuta el código en Main ahora mismo se tiran 5 dados de manera aleatoria.

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,10 +1,18 @@
 package controller;
 
+import model.Dados;
+import model.Jugador;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Dados dado1 = new Dados();
+		System.out.println(dado1.getValorDado());
+		dado1.tirarDado();
 
+		Jugador jugador1 = new Jugador("Paco");
+		jugador1.tirarDados();
 	}
 
 }
--- src/model/Jugador.java
+++ src/model/Jugador.java
@@ -1,5 +1,43 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Jugador {
+
+	private String nombre;
+	private ArrayList<Dados> dadosFinalesJugador = new ArrayList<Dados>();
+	private ArrayList<Dados> dadosATirarJugador = new ArrayList<Dados>();
+	private ArrayList<Dados> dadosToChangeJugador = new ArrayList<Dados>();
+	private final int NUMERO_DADOS_JUGADOR = 5;
+
+	public Jugador(String nombre) {
+		this.nombre = nombre;
+
+		// Cuando llamamos al constructor se ejecuta todo el código.
+		// En este caso cuando creamos al jugador ademas llamamos a la función
+		// Donde se instancian 5 dados de valor 1 por defecto.
+		crearDados();
+
+	}
+
+	// MÉTODOS VARIOS
+
+	public void tirarDados() {
+
+		int contador = 1;
+		for (Dados d : dadosATirarJugador) {
+			System.out.print(contador + ".- ");
+			d.tirarDado();
+			contador++;
+		}
+
+	}
+
+	public void crearDados() {
+		for (int i = 0; i < NUMERO_DADOS_JUGADOR; i++) {
+			dadosATirarJugador.add(new Dados());
+		}
+	}
 
 }
