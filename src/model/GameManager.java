28aac0676eca9dd93e31e0bf3807489c0e4943c5
Gustavo <gusparsal@protonmail.com>
2025-04-05 21:26:18 +0200

    máx 13 rondas

--- src/model/GameManager.java
+++ src/model/GameManager.java
@@ -149,7 +149,7 @@ public class GameManager {
 			turno = 0; // vuelve al turno del primer jugador
 			// estadoJuego = false; // con esto solo hay una ronda
 			turnoGlobal++;
-			if (turnoGlobal == 1) { // CAMBIAR A 13 PARA JUEGO COMPLETO
+			if (turnoGlobal == 1) { // CAMBIAR A 13 PARA JUEGO COMPLETO. MÁXIMO 13 RONDAS
 				estadoJuego = false;
 			}
 		}
